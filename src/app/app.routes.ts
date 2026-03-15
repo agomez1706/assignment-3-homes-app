fb9ef6319100a2d91db379681646992deb40b936
agomez1706 <alexjunkmail1706@gmail.com>
2026-03-15 13:29:45 -0500

    Integrated details page into the application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,11 @@ export const routes: Routes = [
   {
     path: '',
     component: Home,
-    title: 'Home page',
+    title: 'Home Page'
   },
   {
-    path: 'details',
+    path: 'details/:id',
     component: Details,
     title: 'Home Details'
-  },
+  }
 ];
